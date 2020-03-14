9d1e171389b70c7964663f4a04db9a2a803d622a
ormaniec <wojciech.ormaniec@gmail.com>
2020-03-15 00:23:52 +0100

    Third refactor step - Add calculator

--- main.cpp
+++ main.cpp
@@ -20,9 +20,45 @@ int mainMenu()
     return userChoice;
 };
 
+int calcMenu()
+{
+    int userChoice;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "CALC MENU" << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "1. Adding" << std::endl;
+    std::cout << "2. Subtracting" << std::endl;
+    std::cout << "3. Multiplication" << std::endl;
+    std::cout << "4. Division" << std::endl;
+    std::cout << "5. Exit" << std::endl;
+    std::cout << "Your choice: ";
+
+    std::cin >> userChoice;
+    return userChoice;
+};
+
+void calculatorLogic(int operationType)
+{
+    int firstNumber, secondNumber, operationResult = 0;
+    std::cout << "Please give two numbers: ";
+    std::cin >> firstNumber >> secondNumber;
+
+    switch(operationType)
+    {
+        case 1: operationResult = firstNumber + secondNumber; break;
+        case 2: operationResult = firstNumber - secondNumber; break;
+        case 3: operationResult = firstNumber * secondNumber; break;
+        case 4: operationResult = firstNumber / secondNumber; break;
+    }
+
+    std::cout << "Operation result is: " << operationResult << std::endl;
+}
+
 int main()
 {
-    int loginAttempts = 0;
+    int  loginAttempts         = 0;
     bool shouldContinueRunning = true;
 
     std::string login, password;
@@ -38,10 +74,36 @@ int main()
 
         if (login == "michal" && password == "behemoth")
         {
-            while(shouldContinueRunning)
+            while (shouldContinueRunning)
             {
+                int choice = mainMenu();
 
-                shouldContinueRunning = false;
+                switch (choice)
+                {
+                    case 1:
+                    {
+                        int userCalcOperation = calcMenu();
+                        if(choice < 5)
+                        {
+                            calculatorLogic(userCalcOperation);
+                            std::this_thread::sleep_for(std::chrono::seconds(2));
+                        }
+                        else
+                            std::cout << "Wrong option!" << std::endl;
+
+                    }
+                        break;
+                        // case 2: {} break;
+                        // case 3: {} break;
+                        // case 4: {} break;
+                    case 5:
+                    {
+                        shouldContinueRunning = false;
+                    }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         else
@@ -59,6 +121,8 @@ int main()
             }
         }
     }
+
+    std::cout << "\n\nProgram shutdown successful.\nYou have been logged out.\n\n\t\t\tThank you for your cooperation!" << std::endl;
 
     return 0;
 }
